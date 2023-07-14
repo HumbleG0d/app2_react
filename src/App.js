b63dce7f2bdafd08d4a3d3065c86e54bf891d3f8
HumbleG0d <sergiouni2000@gmail.com>
2023-07-14 05:32:40 -0500

    add the Button and Counter components for the operation of our app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
+import './normalize.css'
 import './App.css'
+import './components/Boton'
+import { Boton } from './components/Boton'
+import { Contador } from './components/Contador'
+import { useState } from 'react'
 
 function App() {
+
+    const [numClicks , setNumClicks] = useState(0)
+
+    const manejarClick = () => {
+        setNumClicks(numClicks + 1)
+    }
+
+    const reiniciarContador = () => {
+        setNumClicks(0)
+    }
+
     return (
         <div className="App">
-           
+            <div className='contador'>
+                <h1 className='title__contenedor'>Contador de click</h1>
+            </div>
+
+            <div className='contenedor-principal'>
+                <Contador
+                    numClick = {numClicks}
+                />
+                <Boton
+                    texto = 'click'
+                    isBotonClick = {true}
+                    manejarClick={manejarClick}
+                />
+
+                <Boton
+                    texto = 'reiniciar'
+                    isBotonClick = {false}
+                    manejarClick={reiniciarContador}
+                />
+                
+            </div>
         </div>
     )
 }
