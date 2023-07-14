568b391bfa1cbd18fc871032697f9397e0a6397b
HumbleG0d <sergiouni2000@gmail.com>
2023-07-14 05:18:27 -0500

    add style Contador

--- src/components/Contador.js
+++ src/components/Contador.js
@@ -1,6 +1,8 @@
+import '../stylesheets/Contador.css'
+
 export function Contador({numClick}) {
     return (
-        <div className='contador'>
+        <div className='contador_click'>
             {numClick}
         </div>
     )
